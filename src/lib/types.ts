e231d2c02aa29b7a9c19daca1370d7f3297bbe14
Moulik Budhiraja <moulikbudhiraja@gmail.com>
2023-12-20 19:53:49 -0500

    chore: finish resolving merge conflicts

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -3,15 +3,3 @@ export interface User {
   email: string;
   secret: string;
 }
-<<<<<<< HEAD
-
-export enum Status {
-  DEFAULT, // Inputs
-  LOADING, // Shows loading components.
-  ERROR, // Requires inputs. Shows error message at bottom.
-  SUCCESS, // Shows success message
-  ALREADY_REGISTERED, // Requires inputs. Shows error message at bottom.
-  EMPTY_FIELDS, // Requires inputs. Shows error message at bottom.
-}
-=======
->>>>>>> parent of 6702473 (feature: converted from router backend to trpc)
