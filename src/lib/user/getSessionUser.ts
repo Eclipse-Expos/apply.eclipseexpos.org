a55063c0c512e6313d53f6093d4683b36d442b01
Moulik Budhiraja <moulikbudhiraja@gmail.com>
2024-02-05 21:18:45 -0500

    feat: add autofill when authenticated

---
+++ src/lib/user/getSessionUser.ts
@@ -0,0 +1,21 @@
+"use server";
+
+import { getServerSession } from "next-auth";
+import { prisma } from "../prisma";
+
+export async function getSessionUser() {
+  const session = await getServerSession();
+  const email = session?.user?.email;
+
+  console.log(session);
+
+  if (!email) return null;
+
+  const user = await prisma.user.findUnique({
+    where: {
+      email: email,
+    },
+  });
+
+  return user;
+}
