3f717ccad09b587f4622acedcd893f24622cce0c
Moulik Budhiraja <moulikbudhiraja@gmail.com>
2024-02-15 22:02:04 -0500

    chore: update db migrations

--- prisma/migrations/20240216030149_add_auth/migration.sql
+++ prisma/migrations/20240216030149_add_auth/migration.sql
@@ -1,13 +1,6 @@
-/*
-  Warnings:
-
-  - Added the required column `firstName` to the `User` table without a default value. This is not possible if the table is not empty.
-  - Added the required column `lastName` to the `User` table without a default value. This is not possible if the table is not empty.
-
-*/
 -- AlterTable
-ALTER TABLE "User" ADD COLUMN     "firstName" TEXT NOT NULL,
-ADD COLUMN     "lastName" TEXT NOT NULL;
+ALTER TABLE "User" ADD COLUMN     "firstName" TEXT NOT NULL DEFAULT '',
+ADD COLUMN     "lastName" TEXT NOT NULL DEFAULT '';
 
 -- CreateTable
 CREATE TABLE "Auth" (
